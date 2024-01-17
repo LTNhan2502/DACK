9836eb1c2b54e8f12195ce5eb32ca7f60ae40bae
LTNhan2502 <firstunio@gmail.com>
2024-01-17 17:16:05 +0700

    update branch adminpage - login

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
@@ -12,6 +14,31 @@ class AdminController extends Controller
     }
 
     public function login(){
+        // User::create([
+        //     'name' => 'Admin Manager',
+        //     'email' => 'letrongnhan2502@gmail.com',
+        //     'password' => bcrypt(123123)
+        // ]);
         return view('admin.login');
+    }
+
+    public function check_login(Request $req){
+        $req->validate([
+            'email' => 'required|email|exists:users',
+            'password' => 'required',
+        ]);
+
+        $data = $req->only('email', 'password');
+        $check = auth()->attempt($data);
+
+        if($check){
+            return redirect()->route('admin.index')->with('success', 'Welcome Back');
+        }
+        return redirect()->back()->with('fail', 'Your email or password does not match');        
+    }
+
+    public function logout(){
+        auth()->logout();
+        return redirect()->route('admin.login')->with('fail', 'Logged out');
     }
 }
