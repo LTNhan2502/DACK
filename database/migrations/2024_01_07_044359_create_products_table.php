c500c93e6ab3fad920b64cfefa7d64899999c60d
LTNhan2502 <firstunio@gmail.com>
2024-02-06 22:07:05 +0700

    Update branch adminpage 06022024

--- database/migrations/2024_01_07_044359_create_products_table.php
+++ database/migrations/2024_01_07_044359_create_products_table.php
@@ -17,14 +17,15 @@ return new class extends Migration
             $table->bigIncrements('id');
             $table->string('name', 255);
             $table->float('price');
+            $table->float('sale_price');
             $table->string('img', 150);
             $table->text('content');
-            $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id')
-            ->references('id')
-            ->on('nguoi_dungs')
-            ->onUpdate('cascade')
-            ->onDelete('cascade');
+            // $table->unsignedBigInteger('user_id');
+            // $table->foreign('user_id')
+            // ->references('id')
+            // ->on('nguoi_dungs')
+            // ->onUpdate('cascade')
+            // ->onDelete('cascade');
             $table->unsignedBigInteger('category_id');
             $table->foreign('category_id')
             ->references('id')
--- resources/views/admin/product/index.blade.php
+++ resources/views/admin/product/index.blade.php
@@ -9,7 +9,7 @@
         <button type="submit" class="btn btn-primary">
             <i class="fa fa-search"></i>
         </button>
-        <a href="{{ route('product.create') }}" class="btn btn-primary pull-right">
+        <a href="{{ route('products.create') }}" class="btn btn-primary pull-right">
             <i class="fa fa-plus"></i>Add new
         </a>
     </form>
@@ -31,14 +31,17 @@
                 <tr>
                     <td scope="row">{{ $loop->index + 1 }}</td>
                     <td>
-                        <img src="public/img/{{ $model->img }}" width="40px" alt="">
+                        <img src="uploads/product/{{ $model->img }}" width="40px" alt="">
                     </td>
                     <td>{{ $model->name }}</td>
-                    <td>{{ $model->price }}</td>
+                    <td>
+                        {{ $model->price }} 
+                        <span class="badge text-bg-secondary">{{ $model->sale_price }}</span> 
+                    </td>
                     <td>{{ $model->content }}</td>
                     <td>{{ $model->category->name }}</td>
                     <td class="text-right">
-                        <a href="{{ route('product.edit', 1) }}" class="btn btn-sm btn-primary">
+                        <a href="{{ route('products.edit', 1) }}" class="btn btn-sm btn-primary">
                             <i class="fa fa-edit"></i>Edit
                         </a>
                         <a href="" class="btn btn-sm btn-danger">
